f99ffc8bf923d930461c1558f9712cac05b2cc80
Joeri Verhavert <joeri.verhavert@telenetgroup.be>
2026-02-26 13:48:14 +0100

    Refactor: Simplify flexbox styling for `.tools-built-with` by removing specific media query overrides and applying general flex properties.

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	logFile, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logFile, err := os.OpenFile("/logs/access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("Failed to open log file: %v", err)
 	}
