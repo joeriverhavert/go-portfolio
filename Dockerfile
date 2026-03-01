c4183b988d603337ff8fbd2149033132a97f5c16
Joeri Verhavert <joeri.verhavert@telenetgroup.be>
2026-03-01 10:09:47 +0100

    feat: Enhance application security and robustness with multi-stage Docker builds, distroless images, strict Kubernetes security contexts, and increased deployment replicas.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,24 @@
-FROM golang:1.25.6-alpine
+# Build stage
+FROM golang:1.25.6-alpine AS BUILD
 
 WORKDIR /app
 
-COPY go.mod /app/
+COPY go.mod ./
 RUN go mod download
 
-COPY public /app/public
-COPY internal /app/internal
-COPY main.go /app/main.go
+COPY . .
 
-RUN CGO_ENABLED=0 GOOS=linux go build -o golang-portfolio .
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o golang-portfolio .
+
+# Runtime stage
+FROM gcr.io/distroless/base-debian12
+
+WORKDIR /app
+
+COPY --from=BUILD /app/golang-portfolio /app/golang-portfolio
+
+USER 65532:65532
 
 EXPOSE 80
 
-CMD ["./golang-portfolio"]
+CMD ["/app/golang-portfolio"]
--- main.go
+++ main.go
@@ -1,29 +1,17 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"portfolio/internal/logging"
 )
 
 func main() {
-	logFile, err := os.OpenFile("/logs/access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("Failed to open log file: %v", err)
-	}
-	defer logFile.Close()
-
-	multiWriter := io.MultiWriter(os.Stdout, logFile)
-	logging.SetOutput(multiWriter)
-
-	// Serve static files from the "public" directory
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/", logging.LoggingMiddleware(fs))
 
 	log.Println("Server started on :80")
-	err = http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
