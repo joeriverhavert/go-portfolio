e3304bed2d97071e4ed2beecd31aacd473601409
Joeri Verhavert <joeri.verhavert@telenetgroup.be>
2026-03-04 10:46:23 +0100

    feat: Embed static assets into the Go binary and update a testimonial on the homepage.

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 	"net/http"
 	"portfolio/internal/logging"
 )
 
+//go:embed public/*
+var publicFS embed.FS
+
 func main() {
-	fs := http.FileServer(http.Dir("./public"))
+	fsys, err := fs.Sub(publicFS, "public")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs := http.FileServer(http.FS(fsys))
 	http.Handle("/", logging.LoggingMiddleware(fs))
 
 	log.Println("Server started on :80")
-	err := http.ListenAndServe(":80", nil)
+	err = http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
